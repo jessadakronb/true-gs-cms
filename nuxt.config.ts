1a4065f620e6e161848ec05a6238bb940e5ea3fa
jessadakron.b <jessadakron.b@integra8t.com>
2023-11-16 14:32:32 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+export default defineNuxtConfig({
+  ssr: false,
+  typescript: {
+    shim: false,
+  },
+  
+  app: {
+    head: {
+      title:
+        "MaterialPro Free Nuxt 3 Dashboard",
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  nitro: {
+    serveStatic: true,
+  },
+  sourcemap: { server: false, client: false },
+  devServerHandlers: [],
+});
